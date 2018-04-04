d8a43c389200287ce1fd3b5400c58f5bce7770dc
Mariana Neto <32868723+bluediberry@users.noreply.github.com>
2018-04-04 12:11:43 +0100

    change

--- procdic/src/headlines.cpp
+++ procdic/src/headlines.cpp
@@ -2,6 +2,7 @@
 #include <cstring>
 
 int Headlines(char letra, string dictionary_file) {
+  //determinar os simbolos aceitaveiss
 //percorrer o ficheiro do dicionario pela letra indicada
 //cada vez que for encontrada uma headline aumenta o contador
 //se o resultado da divisao por 100 for 0 escrever um .
