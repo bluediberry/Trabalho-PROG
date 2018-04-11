becadc1eee1212064f992a0dc85b6649027e1b86
Mariana Neto <32868723+bluediberry@users.noreply.github.com>
2018-04-11 16:41:45 +0100

    ..

--- procdic/src/main.cpp
+++ procdic/src/main.cpp
@@ -10,45 +10,63 @@
 using namespace std;
 
 
-int headlines(string letra)
+
+int headlines(vector<string> &words, string line)
  {
-	string dictionary_file;
-  ifstream f;
- 	string line;
- 	int lineCount;
+   // is a headline?
+   for(int i = 0; i < line.length(); i++)
+    if(line[i] < 'A' || line[i] > 'Z' || line[i] != ';' || line[i] != ' ')
+      return;
 
-// cout << "Dictionary file ? ";
-  	cin >> dictionary_file;
+  // keep processing
+  //ABLE; ABC; ABBA
+  string newword = "";
+  for(int i = 0; i < line.length(); i++) {
 
-  	f.open(dictionary_file);
-    if (!f.is_open())
-    {
-      cerr << "File " << dictionary_file << " not found!\n";
-      return(1);
-    }
 
-  	cout << "FILE CONTENTS:\n";
-  	lineCount = 0;
+// se caracter for letra
+  // word = word + Hletra
 
-  		while (!f.eof())
-  	{
+  // else se caracter for ; ou espaço e word nao vazia
+    // adicojnar word, apagara woerd
+
+  // else se caracter for ; ou esapclo, e word vazia
+    // continue;
+
+
+    // se line i for letr
+      newword = newword + line[i];
+
+      // quando line i for ; entao push newoord, newword = ""
+
+  }
+
+  //words.push_back(coisa)
+
+/*
+
+      while (!f.eof()) {
   		getline(f, line);
   		if(line.compare(line.length() - 1 - (line.length()-1), 1, letra) == 0) {
-  		//cout << line.length() << " line length" << endl;
   		cout << "LINE " << ++lineCount << " -> " << line << endl;
   	}
-  	}
-
-  		//close the file
-  		f.close();
-      return 0;
+  }
+*/
+return 0;
 
 }
 
-int numHeadlines(string letra)
+int numHeadlines(string Hletra)
 
 {
+  lastline = linecount/100
 
+/*  for(i=0; i <= linecount;  i++) {
+    if(i%100 == 0)
+      cout << ".";
+      i++;
+  }
+  */
       return 0;
 
 }
@@ -67,9 +85,49 @@ int main() {
 	cout << "EXTRACTION OF WORLD LIST FROM DICTIONARY" << endl;
 	cout << "==========================================" << endl << endl;
   cout << "Dictionary file ? ";
+  cin >> dictionary_file;
 
-  headlines("A");
-  cout << endl;
+  f.open(dictionary_file);
+  if (!f.is_open())
+  {
+    cerr << "File " << dictionary_file << " not found!\n";
+    return(1);
+  }
+
+  cout << "FILE CONTENTS:\n";
+  lineCount = 0;
+
+  vector<string> words;
+
+  while (!f.eof()) {
+    getline(f, line);
+    headlines(words, line);
+  //if(line.compare(line.length() - 1 - (line.length()-1), 1, "A") == 0) {
+  //cout << "LINE " << ++lineCount << " -> " << line << endl;
+  }
+
+  // words vai conter todas as apalvras simples
+
+  // step 2; tirar dupkicados
+
+//for i < numwords
+  //for j < numwords
+    // if word[i]  == word[j]
+      // words.erase(j)
+      //  ctr
+
+   words.sort(0, words.size());
+
+   // step 4
+   // for i < words.size()
+    // fout << words[i]
+}
+
+
+
+  //close the file
+  f.close();
+
 
 	cout << "Word list file ? ";
 	cin >> word_list_file;
@@ -80,7 +138,8 @@ int main() {
 	cout << "beginning with letter ... " << endl << endl;
 
 	cout << "A" << endl;
-	cout << "B" << endl;
+  numHeadlines("A");
+  cout << "B" << endl;
 	cout << "C" << endl;
 	cout << "D" << endl;
 	cout << "E" << endl;
