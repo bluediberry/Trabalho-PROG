e7e91a9478510ea817a5f64bbc86b1e8b260583d
Mariana Neto <32868723+bluediberry@users.noreply.github.com>
2018-04-13 22:20:15 +0100

    .

--- playwords/src/main.cpp
+++ playwords/src/main.cpp
@@ -8,27 +8,27 @@
 
 using namespace std;
 
-//Ordena o vetor de words
 void sort(vector<string> &v1)
 {
 	sort(v1.begin(), v1.end());
 }
 
-//remove palavras duplicadas do vetor de words
 void remove_duplicates(vector<string> &v1)
 {
   v1.erase(unique(v1.begin(), v1.end()), v1.end());
 }
 
 
-//recebe linhas e procede ao tratamento das mesmas segundo as regras de headlines
 void headlines(vector<string> &words, string line)
 
  {
+  //string newword = "";
+  //words.push_back(line);
+
   for(int i = 0; i < line.length(); i++) {
 
 					string newword = "";
-
+//    if(line[i] < 'A' || line[i] > 'Z' || line[i] != ';' || line[i] != ' ')
     if(line[i] < 'A' || line[i] > 'Z' || line[i] != ';' || line[i] != ' ') {
         i++;
         for(int i = 0; i < line.length(); i++)
@@ -96,7 +96,6 @@ int main() {
   cout << "Dictionary file ? ";
   cin >> dictionary_file;
 
-//abrir a file do dicionario
   f.open(dictionary_file);
   if (!f.is_open())
   {
@@ -116,7 +115,6 @@ int main() {
 	cout << "Word list file ? ";
 	cin >> word_list_file;
 
-//abrir a file onde sera guardada a lista final
   fo.open(word_list_file);
   if (!fo.is_open())
   {
@@ -129,9 +127,8 @@ int main() {
 
 	cout << "Extracting simple words from file " << dictionary_file << ", " << endl;
 	cout << "beginning with letter ... " << endl << endl;
-//fazer cout de . por cada 100 headlines de cada letra
-//nao completo
-/*		for (int i = 0; i < 26; i++) {
+
+		for (int i = 0; i < 26; i++) {
 				cout << endl << (char) (65 + i) << endl;
 					int wordCount = 0;
 						for (int i=0 ; i < words.size(); i++) {
@@ -140,17 +137,16 @@ int main() {
 											cout << ".";
 											}
 											}
-										}*/
+											}
 	cout << endl;
 	cout << "Number of simple words = " << words.size() - 1  << endl;
 	cout << "Sorting words ... " << endl;
-  sort(words); //ordenar vetor de words
+  sort(words);
 	cout << "Removing duplicate words ..." << endl;
-  remove_duplicates(words); //remover duplicados do vetor ordenado
+  remove_duplicates(words);
 	cout << "Number of non-duplicate simple words = " << words.size() - 1 << endl;
 
 	cout << "Saving words into file " << word_list_file << " ..." << endl;
-	//escrever na file designada o vetor de words final
       for (int i=0; i < words.size(); i++) {
         fo << words[i] << endl;
         }
